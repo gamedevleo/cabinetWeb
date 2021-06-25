5a7707e11501ebeb3c8f6c01c7467b63469438ff
kaiyangli <gamedevleo@gmail.com>
2021-06-25 13:45:47 +1000

    Homepage change 1

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -19,17 +19,7 @@ const HomePage =()=>{
         <img src={headerImg} alt=''/>
       </header>
       <div className='homepage_content'>
-        <div className='homepage_classification'>
-          <div className='homepage_classification_title'>
-            <h3>CLASSIFICATION</h3>
-          </div>
-          <div className='homepage_classification_items'>
-            <Card img={BBQImg} title='BBQ'/>
-            <Card img={wardrobeImg} title='Wardrobe'/>
-            <Card img={kitchenImg} title='Kitchen'/>
-            <Card img={wineCabinetImg} title='Wine Cabinet'/>
-          </div>
-        </div>
+        
       </div>
       <Footer />
     </div>
