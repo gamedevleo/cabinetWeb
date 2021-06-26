c11a8e913bcd6172f4a97e7d5919acc85b9a1d50
Kaiyang Li <gamedevleo@gmail.com>
2021-06-26 22:56:58 +1000

    Fix conflict

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Info from './Info';
 import './CSS/Footer.css';
-<<<<<<< HEAD
 import BusinessCard from './BusinessCard';
 
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
@@ -9,17 +8,6 @@ const Footer = ()=>{
   return (
     <div className='footer'>
       <BusinessCard />
-      
-=======
-
-const Footer = ()=>{
-  return (
-    <div className='footer'>
-      <Info title='Phone:' content='0497 611111'/>
-      <Info title=' ' content='0497 655555'/>
-      <Info title='Email:' content='inhomecabinet@gmail.com' />
-      <Info title='Address:' content='12 Bellfrog Street, Greenacre, NSW 2190' />
->>>>>>> 78918d35aaec3a104974faecc1762464f4275759
     </div>
   )
 }
